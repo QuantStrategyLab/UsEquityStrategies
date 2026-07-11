QuantStrategyLab/HkEquityStrategies

fix(ci): persist validated lifecycle baselines
Co-Authored-By: Codex <noreply@openai.com>

scripts/run_walk_forward_backtest.py
@@ -4,12 +4,18 @@
 from __future__ import annotations
 
 import argparse
+import copy
+import hashlib
 import json
+import tempfile
 from datetime import date
 from pathlib import Path
 from typing import Any
 
+import pandas as pd
+
 from hk_equity_strategies.backtest.orchestrator_runner import SUPPORTED_PROFILES, build_backtest_runner
+from hk_equity_strategies.backtest.orchestrator_runner import _synthetic_market_history as _runner_synthetic_market_history
 from hk_equity_strategies.strategies.hk_equity_combo import PROFILE_NAME as HK_EQUITY_COMBO_PROFILE
 from hk_equity_strategies.strategies.hk_global_etf_tactical_rotation import DEFAULT_MIN_HISTORY_DAYS
 
@@ -25,6 +31,7 @@ PROFILE_DEFAULTS: dict[str, dict[str, Any]] = {
         "combo_mode": "dynamic",
     },
 }
+MIN_SYNTHETIC_DAYS = 700
 
 
 def _result_payload(item: Any) -> dict[str, Any]:
@@ -40,13 +47,53 @@ def _result_payload(item: Any) -> dict[str, Any]:
     }
 
 
+def _baseline_param_set_id(
+    profile: str,
+    params: dict[str, Any],
+) -> str:
+    fingerprint = hashlib.sha256(json.dumps(params, sort_keys=True, default=str).encode("utf-8")).hexdigest()[:12]
+    return f"{profile}_baseline_{fingerprint}"
+
+
+def _baseline_identity_params(
+    params: dict[str, Any],
+    *,
+    synthetic_days: int | None,
+) -> dict[str, Any]:
+    identity = copy.deepcopy(params)
+    if synthetic_days is not None:
+        identity["_synthetic_days"] = synthetic_days
+    return identity
+
+
+def _clone_market_history(market_history: pd.DataFrame | None) -> pd.DataFrame | None:
+    return market_history.copy(deep=True) if market_history is not None else None
+
+
+def _shared_market_history(params: dict[str, Any], synthetic_days: int, market_history: pd.DataFrame | None) -> pd.DataFrame:
+    if market_history is not None:
+        return _clone_market_history(market_history)
+    min_history_days = int(params.get("min_history_days", DEFAULT_MIN_HISTORY_DAYS))
+    if int(synthetic_days) < min_history_days:
+        raise ValueError(f"synthetic_days must be >= {min_history_days} for profile={params!r}")
+    return _runner_synthetic_market_history(days=int(synthetic_days))
+
+
+def _build_runner(*, profile: str, synthetic_days: int, market_history: pd.DataFrame | None = None):
+    return build_backtest_runner(
+        profile,
+        market_history=market_history,
+        synthetic_days=synthetic_days,
+    )
+
+
 def run_walk_forward(
     *,
     profile: str,
     windows: tuple[tuple[date, date], ...] = DEFAULT_WINDOWS,
     synthetic_days: int = 700,
     store_root: Path | None = None,
-    market_history: Any = None,
+    market_history: pd.DataFrame | None = None,
 ) -> dict[str, Any]:
     from quant_platform_kit.strategy_lifecycle.backtest_orchestrator import BacktestOrchestrator
     from quant_platform_kit.strategy_lifecycle.performance_store import PerformanceStore
@@ -55,27 +102,69 @@ def run_walk_forward(
         raise ValueError(f"unsupported profile={profile!r}; supported={sorted(SUPPORTED_PROFILES)}")
 
     params = dict(PROFILE_DEFAULTS.get(profile, {"min_history_days": DEFAULT_MIN_HISTORY_DAYS}))
-    runner = build_backtest_runner(
-        profile,
-        market_history=market_history,
-        synthetic_days=synthetic_days,
+    store_root = store_root or Path("/tmp/hk_equity_wf_store")
+    store_root.mkdir(parents=True, exist_ok=True)
+    baseline_params = copy.deepcopy(params)
+    shared_market_history = _shared_market_history(baseline_params, synthetic_days, market_history)
+    with tempfile.TemporaryDirectory(prefix=f"{profile}_wf_", dir=store_root) as scratch_dir:
+        scratch_store = PerformanceStore(local_root=Path(scratch_dir))
+        scratch_orchestrator = BacktestOrchestrator(store=scratch_store)
+        scratch_orchestrator.register_runner(
+            "hk_equity",
+            _build_runner(
+                profile=profile,
+                market_history=_clone_market_history(shared_market_history),
+                synthetic_days=synthetic_days,
+            ),
+        )
+        baseline_runner = _build_runner(
+            profile=profile,
+            market_history=_clone_market_history(shared_market_history),
+            synthetic_days=synthetic_days,
+        )
+        baseline_raw = baseline_runner.run(
+            profile,
+            copy.deepcopy(baseline_params),
+            start_date=None,
+            end_date=None,
+        )
+        via_orch = scratch_orchestrator.run(
+            profile,
+            domain="hk_equity",
+            params=copy.deepcopy(baseline_params),
+            param_set_id=f"{profile}_full_compare",
+            start_date=None,
+            end_date=None,
+        )
+        wf_params = copy.deepcopy(baseline_params)
+        wf_results = scratch_orchestrator.walk_forward(
+            profile,
+            domain="hk_equity",
+            params=wf_params,
+            windows=windows,
+            param_set_id=f"{profile}_wf",
+        )
+    baseline_store_params = _baseline_identity_params(
+        baseline_params,
+        synthetic_days=synthetic_days if market_history is None else None,
     )
-    store = PerformanceStore(local_root=store_root or Path("/tmp/hk_equity_wf_store"))
+    store = PerformanceStore(local_root=store_root)
     orchestrator = BacktestOrchestrator(store=store)
-    orchestrator.register_runner("hk_equity", runner)
-
-    baseline = runner.run(profile, params)
-    wf_results = orchestrator.walk_forward(
-        profile,
+    baseline = orchestrator.persist_result(
+        baseline_raw,
+        strategy_profile=profile,
         domain="hk_equity",
-        params=params,
-        windows=windows,
-        param_set_id=f"{profile}_wf",
+        params=baseline_params,
+        param_set_id=_baseline_param_set_id(
+            profile,
+            baseline_store_params,
+        ),
     )
     return {
         "strategy_profile": profile,
         "domain": "hk_equity",
         "baseline": _result_payload(baseline),
+        "orchestrator_full_window": _result_payload(via_orch),
         "walk_forward_folds": [_result_payload(item) for item in wf_results],
         "source": "BacktestOrchestrator.walk_forward",
     }

tests/test_drift_workflow_config.py
@@ -1,0 +1,15 @@
+from pathlib import Path
+
+
+def test_drift_workflow_wires_snapshot_repo_and_lifecycle_env() -> None:
+    workflow = (Path(__file__).resolve().parents[1] / ".github" / "workflows" / "drift-check.yml").read_text(encoding="utf-8")
+
+    assert "uses: QuantStrategyLab/QuantPlatformKit/.github/workflows/reusable-drift-check.yml@335c7a22bc3f570bd5705427ccc40172eda6b289" in workflow
+    assert "strategy_domain: hk_equity" in workflow
+    assert "caller_event_name: ${{ github.event_name }}" in workflow
+    assert "caller_pr_head_repository: ${{ github.event.pull_request.head.repo.full_name || '' }}" in workflow
+    assert "snapshot_repository: QuantStrategyLab/HkEquitySnapshotPipelines" in workflow
+    assert "snapshot_checkout_path: external/HkEquitySnapshotPipelines" in workflow
+    assert "ai_gateway_service_url: ${{ vars.AI_GATEWAY_SERVICE_URL }}" in workflow
+    assert "codex_audit_service_url: ${{ secrets.CODEX_AUDIT_SERVICE_URL }}" in workflow
+    assert "snapshot_repository_token: ${{ secrets.SNAPSHOT_REPOSITORY_TOKEN }}" in workflow

QuantStrategyLab/UsEquityStrategies

scripts/run_walk_forward_backtest.py
@@ -4,12 +4,18 @@
 from __future__ import annotations
 
 import argparse
+import copy
+import hashlib
 import json
+import tempfile
 from datetime import date
 from pathlib import Path
 from typing import Any
 
+import pandas as pd
+
 from us_equity_strategies.backtest.orchestrator_runner import SUPPORTED_PROFILES, build_backtest_runner
+from us_equity_strategies.backtest.orchestrator_runner import _synthetic_market_history as _runner_synthetic_market_history
 from us_equity_strategies.strategies.global_etf_rotation import DEFAULT_MIN_HISTORY_DAYS, PROFILE_NAME
 from us_equity_strategies.strategies.us_equity_combo import PROFILE_NAME as US_EQUITY_COMBO_PROFILE
 
@@ -25,6 +31,7 @@ PROFILE_DEFAULTS: dict[str, dict[str, Any]] = {
         "combo_mode": "dynamic",
     },
 }
+MIN_SYNTHETIC_DAYS = 900
 
 
 def _result_payload(item: Any) -> dict[str, Any]:
@@ -38,6 +45,33 @@ def _result_payload(item: Any) -> dict[str, Any]:
         "observation_count": item.observation_count,
         "run_id": getattr(item, "run_id", None),
     }
+
+
+def _baseline_param_set_id(profile: str, params: dict[str, Any], *, synthetic_days: int) -> str:
+    identity = {
+        "params": params,
+        "synthetic_days": synthetic_days,
+    }
+    fingerprint = hashlib.sha256(json.dumps(identity, sort_keys=True, default=str).encode("utf-8")).hexdigest()[:12]
+    return f"{profile}_baseline_{fingerprint}"
+
+
+def _build_runner(*, profile: str, synthetic_days: int):
+    return build_backtest_runner(profile, synthetic_days=synthetic_days)
+
+
+def _clone_market_history(market_history: pd.DataFrame) -> pd.DataFrame:
+    return market_history.copy(deep=True)
+
+
+def _shared_market_history(profile: str, params: dict[str, Any], synthetic_days: int) -> pd.DataFrame:
+    min_history_days = int(params.get("min_history_days", DEFAULT_MIN_HISTORY_DAYS))
+    if int(synthetic_days) < min_history_days:
+        raise ValueError(f"synthetic_days must be >= {min_history_days} for profile={profile!r}")
+    return _runner_synthetic_market_history(
+        days=int(synthetic_days),
+        include_combo_proxies=profile == US_EQUITY_COMBO_PROFILE,
+    )
 
 
 def run_walk_forward(
@@ -54,23 +88,66 @@ def run_walk_forward(
         raise ValueError(f"unsupported profile={profile!r}; supported={sorted(SUPPORTED_PROFILES)}")
 
     params = dict(PROFILE_DEFAULTS.get(profile, {"min_history_days": DEFAULT_MIN_HISTORY_DAYS}))
-    runner = build_backtest_runner(profile, synthetic_days=synthetic_days)
-    store = PerformanceStore(local_root=store_root or Path("/tmp/us_equity_wf_store"))
-    orchestrator = BacktestOrchestrator(store=store)
-    orchestrator.register_runner("us_equity", runner)
-
-    baseline = runner.run(profile, params)
-    wf_results = orchestrator.walk_forward(
+    store_root = store_root or Path("/tmp/us_equity_wf_store")
+    store_root.mkdir(parents=True, exist_ok=True)
+    baseline_params = copy.deepcopy(params)
+    shared_market_history = _shared_market_history(profile, baseline_params, synthetic_days)
+    baseline_runner = build_backtest_runner(
         profile,
+        synthetic_days=synthetic_days,
+        market_history=_clone_market_history(shared_market_history),
+    )
+    baseline_raw = baseline_runner.run(
+        profile,
+        copy.deepcopy(baseline_params),
+        start_date=None,
+        end_date=None,
+    )
+    with tempfile.TemporaryDirectory(prefix=f"{profile}_wf_", dir=store_root) as scratch_dir:
+        scratch_store = PerformanceStore(local_root=Path(scratch_dir))
+        scratch_orchestrator = BacktestOrchestrator(store=scratch_store)
+        scratch_orchestrator.register_runner(
+            "us_equity",
+            build_backtest_runner(
+                profile,
+                synthetic_days=synthetic_days,
+                market_history=_clone_market_history(shared_market_history),
+            ),
+        )
+        via_orch = scratch_orchestrator.run(
+            profile,
+            domain="us_equity",
+            params=copy.deepcopy(baseline_params),
+            param_set_id=f"{profile}_full_compare",
+            start_date=None,
+            end_date=None,
+        )
+        wf_params = copy.deepcopy(baseline_params)
+        wf_results = scratch_orchestrator.walk_forward(
+            profile,
+            domain="us_equity",
+            params=wf_params,
+            windows=windows,
+            param_set_id=f"{profile}_wf",
+        )
+    store = PerformanceStore(local_root=store_root)
+    orchestrator = BacktestOrchestrator(store=store)
+    baseline = orchestrator.persist_result(
+        baseline_raw,
+        strategy_profile=profile,
         domain="us_equity",
-        params=params,
-        windows=windows,
-        param_set_id=f"{profile}_wf",
+        params=baseline_params,
+        param_set_id=_baseline_param_set_id(
+            profile,
+            baseline_params,
+            synthetic_days=synthetic_days,
+        ),
     )
     return {
         "strategy_profile": profile,
         "domain": "us_equity",
         "baseline": _result_payload(baseline),
+        "orchestrator_full_window": _result_payload(via_orch),
         "walk_forward_folds": [_result_payload(item) for item in wf_results],
         "source": "BacktestOrchestrator.walk_forward",
     }

tests/test_drift_workflow_config.py
@@ -1,0 +1,15 @@
+from pathlib import Path
+
+
+def test_drift_workflow_wires_snapshot_repo_and_lifecycle_env() -> None:
+    workflow = (Path(__file__).resolve().parents[1] / ".github" / "workflows" / "drift-check.yml").read_text(encoding="utf-8")
+
+    assert "uses: QuantStrategyLab/QuantPlatformKit/.github/workflows/reusable-drift-check.yml@335c7a22bc3f570bd5705427ccc40172eda6b289" in workflow
+    assert "strategy_domain: us_equity" in workflow
+    assert "caller_event_name: ${{ github.event_name }}" in workflow
+    assert "caller_pr_head_repository: ${{ github.event.pull_request.head.repo.full_name || '' }}" in workflow
+    assert "snapshot_repository: QuantStrategyLab/UsEquitySnapshotPipelines" in workflow
+    assert "snapshot_checkout_path: external/UsEquitySnapshotPipelines" in workflow
+    assert "ai_gateway_service_url: ${{ vars.AI_GATEWAY_SERVICE_URL }}" in workflow
+    assert "codex_audit_service_url: ${{ secrets.CODEX_AUDIT_SERVICE_URL }}" in workflow
+    assert "snapshot_repository_token: ${{ secrets.SNAPSHOT_REPOSITORY_TOKEN }}" in workflow